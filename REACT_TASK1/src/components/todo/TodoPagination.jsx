b27cd67f3ad4fcb641c4f26c2d93250fedf5d77b
manohari1990 <manu.manohari90@gmail.com>
2026-07-21 21:26:37 +0530

    Next & prev in Pagination

--- REACT_TASK1/src/components/todo/TodoPagination.jsx
+++ REACT_TASK1/src/components/todo/TodoPagination.jsx
@@ -1,23 +1,25 @@
 /*
-Edge Case 7 - <Prev> 1 2 3 .... 10 11<Next> - Need to work on
+Edge Case 7 - <Prev> 1 2 3 .... 10 11<Next> - In-progress - Left with Next & prev
 Edge Case 8 - the next should not be shown if the user is already on last page- need to work on
 Edge Case 9 - the prev should not be shown if the user is already on page 1- need to work on
 Edge Case 10 - it should handle but with fail safe.. if the 5000 is not exists then should not break UI - need to work on
 */
 
 
-function TodoPagination({currentPage, handlePage, displayPages}){
+function TodoPagination({currentPage, totalPages, handlePage, displayPages}){
     return(
         <div className="row_content">
-        {
-            displayPages.length > 0 && displayPages.map((i, index)=>{
-                if (i === '...') return <span  key={`ellipse-${index}`}>...</span>
-                else return <button className={`primary__button ${currentPage == i ? 'active' : ''}`} key={i} onClick={()=>handlePage(i)}>{i}</button>
-            })
-        }
-        {
-            displayPages.length === 0 && <div>No Todos Yet!</div>
-        }
+            <button className={`primary__button ${currentPage===1?'disabled':''}`} onClick={()=>handlePage(currentPage-1)}>Prev</button>
+            {
+                displayPages.length > 0 && displayPages.map((i, index)=>{
+                    if (i === '...') return <span  key={`ellipse-${index}`}>...</span>
+                    else return <button className={`primary__button ${currentPage == i ? 'active' : ''}`} key={i} onClick={()=>handlePage(i)}>{i}</button>
+                })
+            }
+            {
+                displayPages.length === 0 && <div>No Todos Yet!</div>
+            }
+            <button className={`primary__button ${currentPage===totalPages?'disabled':''}`} onClick={()=>handlePage(currentPage+1)}>Next</button>
         </div>
         
     )
