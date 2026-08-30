4a037644284dbc1005380b711f796cd6501c6429
manohari1990 <manu.manohari90@gmail.com>
2026-08-30 18:07:43 +0530

    buildspec for FE deployment CI/CD

--- NODE_TASK1/src/config/database.js
+++ NODE_TASK1/src/config/database.js
@@ -47,7 +47,10 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 // Check DB connection
